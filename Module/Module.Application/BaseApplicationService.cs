efc906122807194e8b027cd414723fabb7fb6fa8
Never、C <caolin>
2016-09-04 21:55:57 +0800

    fix BaseApplicationService<T>

--- Module/Module.Application/BaseApplicationService.cs
+++ Module/Module.Application/BaseApplicationService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Security.Claims;
 using System.Text;
@@ -11,7 +12,33 @@ namespace Module.Application
 {
     public abstract class BaseApplicationService
     {
-        public string GetUserId()
+        protected string GetUserId()
+        {
+            var claimsPrincipal = Thread.CurrentPrincipal as ClaimsPrincipal;
+            if (claimsPrincipal == null)
+                return null;
+            var claimsIdentity = claimsPrincipal.Identity as ClaimsIdentity;
+            if (claimsIdentity == null)
+                return null;
+            return claimsIdentity.GetUserId();
+        }
+    }
+
+    public abstract class BaseApplicationService<T> where T : DbContext
+    {
+        protected T CurrentDb { set; get; }
+
+        protected async Task<int> SaveChangesAsync()
+        {
+            return await CurrentDb.SaveChangesAsync();
+        }
+
+        protected int SaveChanges()
+        {
+            return CurrentDb.SaveChanges();
+        }
+
+        protected string GetUserId()
         {
             var claimsPrincipal = Thread.CurrentPrincipal as ClaimsPrincipal;
             if (claimsPrincipal == null)
