f3115ff34547d22800ded99e213daa9c3259ce0b
曹麟 <caolin>
2016-09-05 12:03:40 +0800

    add ZeroBaseDbContext

---
+++ Module/Module.Core/ZeroBaseDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Module.Core
+{
+    public class ZeroBaseDbContext<TUser, TRole> : IdentityDbContext<TUser, TRole, string, IdentityUserLogin, IdentityUserRole, IdentityUserClaim>
+        where TUser : IdentityUser<string, IdentityUserLogin, IdentityUserRole, IdentityUserClaim>
+        where TRole : IdentityRole<string, IdentityUserRole>
+    {
+
+    }
+}
