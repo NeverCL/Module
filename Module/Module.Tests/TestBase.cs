c6f1d2435e3f9ce1ed6a575c296c8813b52922e8
曹麟 <caolin>
2016-09-05 15:18:05 +0800

    fix tests

---
+++ Module/Module.Tests/TestBase.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Common;
+using Autofac;
+
+namespace Module.Tests
+{
+    public class TestBase
+    {
+        protected IContainer Container { get; set; }
+
+        protected T GetService<T>()
+        {
+            //TODO Container DI?
+            return Container.Resolve<T>();
+        }
+
+        protected DbConnection CreateDbConnection()
+        {
+            return Effort.DbConnectionFactory.CreateTransient();
+        }
+    }
+}
