e397e04db7e8f14a76ec1321e538ffbcb19a9530
曹麟 <caolin>
2016-09-06 12:56:14 +0800

    fix web

---
+++ Module/Module.Web/Scripts/main.js
@@ -0,0 +1,19 @@
+﻿require.config({
+    paths: {
+        jquery: ['jquery-1.9.1.min', '//cdn.bootcss.com/jquery/1.12.4/jquery'],
+        bootstrap: 'bootstrap.min'
+    },
+    shim: {
+        metisMenu: ['bootstrap'],
+        bootstrap: ['jquery']
+    }
+});
+
+require(['css!../content/bootstrap.min', 'css!../Content/metisMenu.min', 'css!../content/font-awesome.min', 'css!../content/site'
+, 'metisMenu'], function () {
+    $('#side-menu').metisMenu();
+});
+
+require(['app'], function () {
+    angular.bootstrap(document, ['myApp']);
+});
