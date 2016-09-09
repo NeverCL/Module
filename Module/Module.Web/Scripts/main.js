f91656e9809a44a6e6264fc12da60dbad73ba5d2
曹麟 <caolin>
2016-09-09 09:45:35 +0800

    fix main

--- Module/Module.Web/Scripts/main.js
+++ Module/Module.Web/Scripts/main.js
@@ -1,18 +1,22 @@
 ﻿require.config({
     paths: {
         jquery: ['jquery-1.9.1.min', '//cdn.bootcss.com/jquery/1.12.4/jquery'],
-        bootstrap: 'bootstrap.min'
+        bootstrap: 'bootstrap.min',
+        datepicker: 'daterangepicker'
     },
     shim: {
         metisMenu: ['bootstrap'],
-        bootstrap: ['jquery', 'cfg']
+        bootstrap: ['jquery', 'cfg'],
+        datepicker: ['bootstrap']
     }
 });
 
-require(['css!../content/bootstrap.min', 'css!../Content/metisMenu.min', 'css!../content/font-awesome.min', 'css!../content/site'
-, 'metisMenu', 'plugins/bs-btn-alert', 'plugins/bs-btn-modal'], function () {
-    $('#side-menu').metisMenu();
-});
+require(['css!../content/bootstrap.min', 'css!../Content/metisMenu.min'
+    , 'css!../content/font-awesome.min', 'css!../content/site'
+    , 'css!../content/daterangepicker', 'datepicker'
+    , 'metisMenu', 'plugins/bs-btn-alert', 'plugins/bs-btn-modal'], function () {
+        $('#side-menu').metisMenu();
+    });
 
 require(['app'], function () {
     angular.bootstrap(document, ['myApp']);
