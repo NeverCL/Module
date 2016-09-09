c0d325c25a08e12c82081d314cfd2b93cede0049
曹麟 <caolin>
2016-09-09 12:40:30 +0800

    fix datepicker

--- Module/Module.Web/Scripts/modules/angular-datepicker.js
+++ Module/Module.Web/Scripts/modules/angular-datepicker.js
@@ -1,10 +1,10 @@
 ﻿(function (root, factory) {
     if (typeof define === 'function' && define.amd) {
-        define(['jquery', 'angular', 'datepicker'], function (jquery) {
-            factory(jquery);
+        define(['jquery', 'moment', 'angular', 'datepicker'], function (jquery, moment) {
+            factory(jquery, moment);
         });
     } else factory(root.jQuery);
-}(window, function ($) {
+}(window, function ($, moment) {
     'use strict';
     angular.module('ng.bs.datepicker', []).constant('datepickerCfg', {
         "locale": {
@@ -43,20 +43,48 @@
         singleDatePicker: true
     }).directive('date', ['datepickerCfg', function (cfg) {
         return {
-            scope: {
-                format: '@',
-                single: '@'
-            },
+            require: 'ngModel',
             link: dateLink
         }
-        function dateLink(scope, ele) {
+        function dateLink(scope, ele, attrs, modelCtrl) {
             var picker = $(ele);
-            var opt = $.extend(cfg, picker.data());
-            if (scope.format)
-                opt.locale.format = scope.format;
-            if (scope.single === 'false')
-                opt.singleDatePicker = false;
-            picker.daterangepicker(opt);
+            var opts = $.extend(cfg, picker.data());
+            if (attrs.single === 'false')
+                opts.singleDatePicker = false;
+            if (attrs.date) {
+                if (attrs.date === 'time') {
+                    opts.locale.format = 'YYYY-MM-DD HH:mm';
+                    opts.timePicker = true;
+                    opts.timePicker24Hour = true;
+                }
+                if (attrs.date === 'times') {
+                    opts.locale.format = 'YYYY-MM-DD HH:mm:ss';
+                    opts.timePicker = true;
+                    opts.timePicker24Hour = true;
+                    opts.timePickerSeconds = true;
+                }
+            }
+            if (attrs.format)
+                opts.locale.format = attrs.format;
+            picker.daterangepicker(opts);
+
+            modelCtrl.$formatters.push(function (objValue) {
+                var f;
+                f = function (date) {
+                    if (!moment.isMoment(date)) {
+                        return moment(date).format(opts.locale.format);
+                    } else {
+                        return date.format(opts.locale.format);
+                    }
+                };
+                if (opts.singleDatePicker && objValue) {
+                    return f(objValue);
+                } else if (objValue && objValue.startDate) {
+                    return [f(objValue.startDate), f(objValue.endDate)].join(opts.locale.separator);
+                } else {
+                    return '';
+                }
+            });
         }
     }]);
 }));
