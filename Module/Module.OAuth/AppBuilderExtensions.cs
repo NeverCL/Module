51beb721f92df5c9fb735fb202c1a0a1096bb66d
Never、C <caolin>
2016-09-11 04:11:43 +0800

    fix oauth

---
+++ Module/Module.OAuth/AppBuilderExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Module.OAuth;
+
+namespace Owin
+{
+    public static class AppBuilderExtensions
+    {
+        public static void UseModuleOAuth(this IAppBuilder app, AccessProvider accessProvider, RefreshProvider refreshProvider)
+        {
+            app.UseOAuthBearerTokens(new OAuthAuthorizationServerOptions
+            {
+                TokenEndpointPath = new PathString("/token"),
+                Provider = accessProvider,
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(14),
+                AllowInsecureHttp = true,
+                RefreshTokenProvider = refreshProvider
+            });
+        }
+
+        public static void UseModuleOAuth(this IAppBuilder app, AccessProvider accessProvider)
+        {
+            UseModuleOAuth(app, accessProvider, new RefreshProvider());
+        }
+
+        public static void UseModuleOAuth(this IAppBuilder app, RefreshProvider refreshProvider)
+        {
+            UseModuleOAuth(app, new AccessProvider(), refreshProvider);
+        }
+    }
+}
