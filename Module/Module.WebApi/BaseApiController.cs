93ee157c2a4420061e8ed2d8dd496d12de8ef20b
Never、C <caolin>
2016-09-12 17:00:41 +0800

    fix api

--- Module/Module.WebApi/BaseApiController.cs
+++ Module/Module.WebApi/BaseApiController.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Filters;
 using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+using Newtonsoft.Json.Linq;
 using NLog;
 
 namespace Module.WebApi
@@ -25,10 +29,44 @@ namespace Module.WebApi
             return User.Identity.Name;
         }
 
+        [AllowAnonymous]
+        public void Login(LoginInfo loginInfo)
+        {
+            var client = new HttpClient();
+            var url = GetCfgValue("ssoUrl");
+            string clientId = GetCfgValue("clientId"), clientSecret = GetCfgValue("clientSecret");
+            var rst = client.PostAsync(url + "api/user/GetUserInfo", new FormUrlEncodedContent(new Dictionary<string, string>
+            {
+                {"token",loginInfo.Token },
+                {"clientId", clientId},
+                {"clientSecret", clientSecret}
+            })).Result.Content.ReadAsStringAsync().Result;
+            var identity = new ClaimsIdentity(new[]
+            {
+                new Claim(ClaimTypes.Name, JObject.Parse(rst)["Id"].Value<string>()),
+                new Claim(ClaimTypes.Name, JObject.Parse(rst)["UserName"].Value<string>()),
+                new Claim(ClaimTypes.GivenName, JObject.Parse(rst)["DisplayName"].Value<string>())
+            }, DefaultAuthenticationTypes.ApplicationCookie);
+            Request.GetOwinContext().Authentication.SignIn(new AuthenticationProperties() { IsPersistent = false }, identity);
+            Redirect(loginInfo.ReturnUrl);
+        }
+
+        protected virtual string GetCfgValue(string name)
+        {
+            return name;
+        }
+
         [HttpGet]
         public string Index()
         {
             return "Hello World";
         }
+    }
+
+    public class LoginInfo
+    {
+        public string Token { get; set; }
+
+        public string ReturnUrl { get; set; }
     }
 }
