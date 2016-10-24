9e4688491c639e7beda31b795c8af7f00f5751fd
Never、C <caolin>
2016-10-24 18:03:31 +0800

    add DisplayName

---
+++ Module/Module.Identity.Cookie/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin.Security;
+
+namespace Module.Identity.Cookie
+{
+    public static class CookieLogin
+    {
+        public static void SignIn(this IAuthenticationManager manager, string userId, string name, string displayName, string authenticationType = "ApplicationCookie")
+        {
+            manager.SignIn(new AuthenticationProperties { IsPersistent = false }, new ClaimsIdentity(new List<Claim>()
+            {
+                new Claim(ClaimTypes.NameIdentifier,userId),
+                new Claim(ClaimTypes.Name,name),
+                new Claim(ClaimTypesConst.DisplayName,displayName),
+            }, authenticationType));
+        }
+
+        //todo get DisplayName
+
+
+    }
+
+
+    public static class ClaimTypesConst
+    {
+        public const string DisplayName = "http://schemas.microsoft.com/ws/2008/06/identity/claims/displayname";
+    }
+}
