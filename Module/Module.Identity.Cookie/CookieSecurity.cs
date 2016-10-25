4f62d9a0de904ffb398a60bbc12fe4352d00ee3e
Never、C <caolin>
2016-10-25 11:16:32 +0800

    fix CookieSecurity

---
+++ Module/Module.Identity.Cookie/CookieSecurity.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using Microsoft.Owin.Security;
+
+namespace Microsoft.Owin.Security
+{
+    /// <summary>
+    /// Identity Cookie
+    /// </summary>
+    public static class CookieSecurity
+    {
+        /// <summary>
+        /// 登录
+        /// </summary>
+        /// <param name="manager"></param>
+        /// <param name="userId"></param>
+        /// <param name="name"></param>
+        /// <param name="displayName"></param>
+        /// <param name="authenticationType"></param>
+        public static void SignIn(this IAuthenticationManager manager, string userId, string name, string displayName = null, string authenticationType = "ApplicationCookie")
+        {
+            if (string.IsNullOrEmpty(displayName))
+                displayName = name;
+            manager.SignIn(new AuthenticationProperties { IsPersistent = false }, new ClaimsIdentity(new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier,userId),
+                new Claim(ClaimTypes.Name,name),
+                new Claim(ClaimTypesConst.DisplayName,displayName),
+            }, authenticationType));
+        }
+    }
+
+    public static class ClaimTypesConst
+    {
+        public const string DisplayName = "http://schemas.microsoft.com/ws/2008/06/identity/claims/displayname";
+    }
+}
---
+++ Module/Module.Identity.Cookie/IdentityExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+using System.Security.Claims;
+using System.Security.Principal;
+using Module.Identity.Cookie;
+
+namespace Microsoft.AspNet.Identity
+{
+    public static class IdentityExtensions
+    {
+        /// <summary>
+        /// 获取昵称
+        /// </summary>
+        /// <param name="identity"></param>
+        /// <returns></returns>
+        public static string GetDisplayName(this IIdentity identity)
+        {
+            ClaimsIdentity claimsIdentity = identity as ClaimsIdentity;
+            if (claimsIdentity != null)
+                return claimsIdentity.FindFirst(ClaimTypesConst.DisplayName).Value;
+            return null;
+        }
+
+        /// <summary>
+        /// 获取昵称
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="identity"></param>
+        /// <returns></returns>
+        public static T GetDisplayName<T>(this IIdentity identity) where T : IConvertible
+        {
+            ClaimsIdentity claimsIdentity = identity as ClaimsIdentity;
+            if (claimsIdentity != null)
+            {
+                string firstValue = claimsIdentity.FindFirst(ClaimTypesConst.DisplayName).Value;
+                if (firstValue != null)
+                    return (T)Convert.ChangeType((object)firstValue, typeof(T), (IFormatProvider)CultureInfo.InvariantCulture);
+            }
+            return default(T);
+        }
+
+    }
+}
