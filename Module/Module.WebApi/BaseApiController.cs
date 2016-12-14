482381541d0083d7e6f9631ddeb6cdbeae583ee0
Never、C <caolin>
2016-12-14 16:36:28 +0800

    add webapi

--- Module/Module.WebApi/BaseApiController.cs
+++ Module/Module.WebApi/BaseApiController.cs
@@ -13,12 +13,30 @@ using Microsoft.Owin.Security;
 using Newtonsoft.Json.Linq;
 using NLog;
 using System.Configuration;
+using System.Threading;
 
 namespace Module.WebApi
 {
+    [ProcessExceptionFilter]
     public abstract class BaseApiController : ApiController
     {
         protected ILogger Logger { get; set; }
+
+        protected ClaimsIdentity GetUser()
+        {
+            var claimsPrincipal = Thread.CurrentPrincipal as ClaimsPrincipal;
+            if (claimsPrincipal == null)
+                return null;
+            var claimsIdentity = claimsPrincipal.Identity as ClaimsIdentity;
+            if (claimsIdentity == null)
+                return null;
+            return claimsIdentity;
+        }
+
+        protected string GetUserId()
+        {
+            return GetUser().GetUserId();
+        }
 
         [HttpGet]
         public string Index()
---
+++ Module/Module.WebApi/ProcessExceptionFilterAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.Filters;
+using NLog;
+using System.Net.Http;
+using System.Net;
+
+namespace Module.WebApi
+{
+    public class ProcessExceptionFilterAttribute : ExceptionFilterAttribute, IExceptionFilter
+    {
+        public override void OnException(HttpActionExecutedContext context)
+        {
+            //The Response Message Set by the Action During Ececution
+            var res = context.Exception.Message;
+            //Define the Response Message
+            HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.InternalServerError)
+            {
+                Content = new StringContent(res)
+            };
+            //Create the Error Response
+            context.Response = response;
+            LogManager.GetLogger(context.ActionContext.ControllerContext.Controller.GetType().Name).Error(context.Exception);
+        }
+    }
+}
